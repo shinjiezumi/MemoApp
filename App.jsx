92e6d7f7ccebd281ba408ec04223d9227f6a70a1
shinjiezumi <shinjiezumi.work@gmail.com>
2021-05-31 21:37:18 +0900

    Add MemoCreateScreen and MemoEditScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
-    // <MemoDetailScreen />
+    <MemoCreateScreen />
   );
 }
